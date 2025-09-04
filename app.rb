7b62c4e499f646389c6b5ea34648e19b7e1ad784
root <root@cubic.local>
2025-09-04 11:48:59 +0300

    fiature: Ответ на часть Rack попытка 2

--- app.rb
+++ app.rb
@@ -1,22 +1,31 @@
 require_relative 'format_time'
-require_relative 'time_handler'
 require 'rack'
 
 class App
   def call(env)
-    Rack::URLMap.new(
-      '/time' => TimeHandler.new,
-      '/' => method(:not_found)
-    ).call(env)
+    @request = Rack::Request.new(env)
+    @format_time = FormatTime.new(@request.params)
+
+    [status, headers, body]
   end
 
   private
 
-  def not_found(_env)
-    [
-      404,
-      { 'Content-Type' => 'text/plain' },
-      ['404 Not Found']
-    ]
+  def status
+    return 404 unless @request.path_info == '/time'
+    return 400 unless @format_time.params_valid?
+    200
+  end
+
+  def body
+    return ["404\n"] unless format_exist?
+
+    @format_time.time
+  end
+
+  def format_exist?
+    return true if @request.params.include?('format')
+
+    false
   end
 end
